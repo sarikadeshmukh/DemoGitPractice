47c99cb02e6b75e59d220f219208654878934328
Sarika.Deshmukh <Sarika.Deshmukh@HG-D110.harbinger.in>
2018-02-05 15:04:19 +0530

    Add string display changes

--- DemoGitPrac/src/DisplayClass.java
+++ DemoGitPrac/src/DisplayClass.java
@@ -1,11 +1,11 @@
 public class DisplayClass {
 
-	String name[]= {"sarika","bhakti","saddam"};
-	public void display() {
-for(String s:name)
-{
-	System.out.println("next string "+s);
-	System.out.println("demo");
-}
-}
+	String name[] = { "sarika", "bhakti", "saddam" };
+
+	public void display(){
+		for(String s : name){
+			System.out.println("next string " + s);
+			System.out.println("demo");
+		}
+	}
 }
