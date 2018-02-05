4e51c40d7eb08a91b2c688510e7f817954703945
Sarika.Deshmukh <Sarika.Deshmukh@HG-D110.harbinger.in>
2018-02-05 15:22:28 +0530

    add some more info for city.

--- DemoGitPrac/src/DisplayClass.java
+++ DemoGitPrac/src/DisplayClass.java
@@ -1,12 +1,16 @@
 public class DisplayClass {
 
 	String name[] = { "sarika", "bhakti", "saddam" };
-
+	String city[] = { "pune", "amt", "Nagpur" };
 	public void display(){
 		for(String s : name){
 			System.out.println("next string " + s);
 			System.out.println("demo");
 			System.out.println("demo2");
+		}
+		for(String c : city){
+			System.out.println("next string " + c);
+		
 		}
 	}
 }
